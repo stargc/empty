504057943160a83357601214f405a5e827fb5e57
guanchen <roseraining2@126.com>
2019-12-21 13:47:04 +0800

    update mybatis generator

--- src/main/java/com/example/empty/EmptyApplication.java
+++ src/main/java/com/example/empty/EmptyApplication.java
@@ -1,5 +1,6 @@
 package com.example.empty;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource(value = {"classpath:config/application.yml", "classpath:config/system-dev.yml"},
 		ignoreResourceNotFound = true)
+@MapperScan("com.example.empty.infrastructure.persistence.mapper")
 public class EmptyApplication {
 
 	public static void main(String[] args) {
